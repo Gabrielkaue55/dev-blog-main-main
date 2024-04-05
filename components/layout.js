916bca32c6ab2e10a9cca526905a80064c49c38d
Gabriel <gkaue9487@gmail.com>
2024-04-05 02:19:57 -0300

    imagem

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/https://blog.digitalcook.fr/wp-content/uploads/2021/01/Intelligence-artificielle.jpg"
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
